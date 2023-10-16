f18b3f93e0f2fa0d6f5db12ec053b36840db23b4
Paolo Bonomi <pbonomi@Paolos-MBP.homenet.telecomitalia.it>
2023-10-16 17:12:05 +0200

    init template

---
+++ shaders/frag.glsl
@@ -0,0 +1,87 @@
+#version 330 core
+
+out vec4 FragColor;
+
+#ifdef GL_ES
+precision mediump float;
+#endif
+
+uniform vec2 u_resolution;
+uniform float u_time;
+
+//shader toy remapping variables
+#define iTime u_time
+
+/*
+    Perlin Noise function
+*/
+
+vec2 random2(vec2 st){
+    st = vec2( dot(st,vec2(127.1,311.7)),
+              dot(st,vec2(269.5,183.3)) );
+    return -1.0 + 2.0*fract(sin(st)*43758.5453123);
+}
+
+// Value Noise by Inigo Quilez - iq/2013
+// https://www.shadertoy.com/view/lsf3WH
+float noise(vec2 st) {
+    vec2 i = floor(st);
+    vec2 f = fract(st);
+
+    vec2 u = f*f*(3.0-2.0*f);
+
+    return mix( mix( dot( random2(i + vec2(0.0,0.0) ), f - vec2(0.0,0.0) ),
+                     dot( random2(i + vec2(1.0,0.0) ), f - vec2(1.0,0.0) ), u.x),
+                mix( dot( random2(i + vec2(0.0,1.0) ), f - vec2(0.0,1.0) ),
+                     dot( random2(i + vec2(1.0,1.0) ), f - vec2(1.0,1.0) ), u.x), u.y);
+}
+
+
+
+void main(void)
+{    
+    vec3 iResolution = vec3(u_resolution.x, u_resolution.y, 0.0);
+    vec2 fragCoord = gl_FragCoord.xy;
+    vec4 fragColor;
+    
+    /*
+    *
+    *
+    *   Copy and paste shadertoy code down here
+    *
+    *                  ___
+    *                  | |
+    *                  | |
+    *               ___| |___
+    *               \       /
+    *                \     /
+    *                 \   /
+    *                  \ /
+    *                   '
+    *
+    */
+
+    // Normalized pixel coordinates (from 0 to 1)
+    vec2 uv = fragCoord/iResolution.xy;
+
+    // Time varying pixel color
+    vec3 col = 0.5 + 0.5*cos(iTime+uv.xyx+vec3(0,2,4));
+
+    // Output to screen
+    fragColor = vec4(col,1.0);
+
+    /*  
+    *
+    *                   .
+    *                  / \
+    *                 /   \
+    *                /     \
+    *               /__   __\
+    *                  | |
+    *                  | |
+    *                  |_|      
+    *
+    */
+
+    FragColor = fragColor;
+}
